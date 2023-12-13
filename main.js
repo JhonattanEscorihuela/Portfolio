9b5dee8ddb0c8f7f880966d7d902fd79e3f7931b
Jhonattan Escorihuela <136757234+JhonattanEscorihuela@users.noreply.github.com>
2023-12-12 23:04:38 -0500

    avance 2 de portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ['Back-End Developer', 'Front-End Developer', 'Full Stack Web Developer', 'Telecommunications Engineer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+});
